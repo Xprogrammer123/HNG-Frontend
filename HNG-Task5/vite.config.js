28cd3dbc436813901c0e37d1901c45cc2b12be15
Xprogrammer123 <fawassaka862@gmail.com>
2026-05-06 19:52:58 +0100

    chore: remove manifest, update CSS dimensions, and reconfigure Vite build for extension assets

--- HNG-Task5/vite.config.js
+++ HNG-Task5/vite.config.js
@@ -1,30 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import path, { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    modulePreload: false,
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'index.html'),
+        background: resolve(__dirname, 'background/background.js'),
+        content: resolve(__dirname, 'content/content.js'),
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          if (chunkInfo.name === 'background' || chunkInfo.name === 'content') {
+            return '[name]/[name].js';
+          }
+          return '[name].js';
+        },
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        popup: path.resolve(__dirname, 'index.html'),
-        background: path.resolve(__dirname, 'background/background.js'),
-        content: path.resolve(__dirname, 'content/content.js'),
-      },
-      output: {
-        entryFileNames: (chunk) => {
-          if (chunk.name === 'background' || chunk.name === 'content') {
-            return '[name]/[name].js';
-          }
-          return 'assets/[name]-[hash].js';
-        },
-      },
     },
   },
 })
